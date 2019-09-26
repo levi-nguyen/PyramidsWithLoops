b39d83b921f1e9ce04eb578612ff8d0840e18959
nguyed4 <nguyed4@LCBLAB2110-50.uccob.uc.edu>
2019-09-25 21:13:30 -0400

    Commit

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,27 +1,29 @@
 /* 
- * Duy Nguyen
+ * Duy Nguyen 
  * nguyed4@mail.uc.eu
  * Printing pyramids using loops. It kind-of works. 
  * What I really want is
-        1
-       222
-      33333
-     4444444
-    555555555  
-*/
+ *     1
+ *    222
+ *   33333
+ *  4444444
+ * 555555555  
+ */
 package main;
 
 public class Main {
-	public static void main(String args[]) {        
-	 for (int i = 1; i < 5; i++) {
-	            for (int j = 0; j < 5 - i; j++) {
-	                System.out.print(" ");
-	            }
-	            for (int k = 0; k <= i; k++) {
-	                System.out.print(k + " ");
-	            }
-	            System.out.println();
-	        }
-	    }
-	}
-
+    public static void main(String args[]) {
+        int lines = 5;
+        for (int i = 0; i < lines; i++) {
+            String space = "";
+            for (int j = 0; j < lines - i ; j++) {
+                space += " ";
+            }
+            System.out.print(space);
+            for (int j = 0; j < 2*i+1; j++) {
+                System.out.print(i+1);
+            }
+            System.out.println();
+        }
+    }
+}
